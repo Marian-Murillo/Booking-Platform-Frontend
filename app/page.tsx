973cb78e6f40cf5e536c3066deb4e4576f622111
marianmurillodesite <marianmurillo-it@desiteproducts.com>
2026-01-19 16:15:45 -0600

    update to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:5000/api/properties");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/properties`);
   const properties = await res.json();
 
   return (
